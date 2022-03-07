db1a87fc9ee3f00ba5bbfb89f991ad69df0eb66f
eulucasm <lucaspoletis@gmail.com>
2022-03-07 19:38:12 -0300

    sessão 3 crie o primeiro app kotlin/android

---
+++ app/src/main/java/com/lucao/orgs/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.lucao.orgs
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : Activity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+
+   }
+}
