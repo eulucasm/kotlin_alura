c4a4c40ef4d2ee5e0281cebf74c2633d04979166
eulucasm <lucaspoletis@gmail.com>
2022-03-14 19:45:19 -0300

    configurações e refatoração

--- app/src/main/java/com/lucao/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/lucao/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,12 +1,38 @@
 package com.lucao.orgs.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lucao.orgs.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.lucao.orgs.databinding.ActivityFormularioProdutoBinding
+import com.lucao.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityFormularioProdutoBinding
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_formulario_produto)
+      setContentView(binding.root)
+
+      binding.botaoSalvar.setOnClickListener {
+         val nome = binding.nomeFormulario.text.toString()
+         val descricao = binding.descricaoFormulario.text.toString()
+         val valor = binding.valorFormulario.text.toString()
+         val valorVerificacao = if (valor.isBlank()) {
+            BigDecimal.ZERO
+         } else {
+            BigDecimal(valor)
+         }
+
+         val produtoNovo = Produto(
+            nome = nome,
+            descricao = descricao,
+            valor = valorVerificacao,
+         )
+         Log.i("FormularioProduto", "onCreate: $produtoNovo")
+
+      }
    }
+
 }
