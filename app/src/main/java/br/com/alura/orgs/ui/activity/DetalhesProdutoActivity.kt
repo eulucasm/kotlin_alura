a90f51c24f614083c42878b5c4ed85ea6ac8032f
eulucasm <lucaspoletis@gmail.com>
2022-04-07 14:23:09 -0300

    Implementando Room

--- app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -1,7 +1,7 @@
 package br.com.alura.orgs.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.alura.orgs.extensions.formataParaMoedaBrasileira
 import br.com.alura.orgs.extensions.tentaCarregarImagem
@@ -9,30 +9,30 @@ import br.com.alura.orgs.model.Produto
 
 class DetalhesProdutoActivity : AppCompatActivity() {
 
-    private val binding by lazy {
-        ActivityDetalhesProdutoBinding.inflate(layoutInflater)
-    }
+   private val binding by lazy {
+      ActivityDetalhesProdutoBinding.inflate(layoutInflater)
+   }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        tentaCarregarProduto()
-    }
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(binding.root)
+      tentaCarregarProduto()
+   }
 
-    private fun tentaCarregarProduto() {
-        intent.getParcelableExtra<Produto>(CHAVE_PRODUTO)?.let { produtoCarregado ->
-            preencheCampos(produtoCarregado)
-        } ?: finish()
-    }
+   private fun tentaCarregarProduto() {
+      intent.getParcelableExtra<Produto>(CHAVE_PRODUTO)?.let { produtoCarregado ->
+         preencheCampos(produtoCarregado)
+      } ?: finish()
+   }
 
-    private fun preencheCampos(produtoCarregado: Produto) {
-        with(binding) {
-            activityDetalhesProdutoImagem.tentaCarregarImagem(produtoCarregado.imagem)
-            activityDetalhesProdutoNome.text = produtoCarregado.nome
-            activityDetalhesProdutoDescricao.text = produtoCarregado.descricao
-            activityDetalhesProdutoValor.text =
-                produtoCarregado.valor.formataParaMoedaBrasileira()
-        }
-    }
+   private fun preencheCampos(produtoCarregado: Produto) {
+      with(binding) {
+         activityDetalhesProdutoImagem.tentaCarregarImagem(produtoCarregado.imagem)
+         activityDetalhesProdutoNome.text = produtoCarregado.nome
+         activityDetalhesProdutoDescricao.text = produtoCarregado.descricao
+         activityDetalhesProdutoValor.text =
+            produtoCarregado.valor.formataParaMoedaBrasileira()
+      }
+   }
 
 }
