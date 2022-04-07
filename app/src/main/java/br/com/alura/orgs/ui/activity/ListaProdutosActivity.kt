7813bffbb8057c15bc0cd5cac840924fb03e1cb2
eulucasm <lucaspoletis@gmail.com>
2022-04-07 15:11:11 -0300

    integrando o room

--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -3,55 +3,74 @@ package br.com.alura.orgs.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
 import br.com.alura.orgs.dao.ProdutosDao
+import br.com.alura.orgs.database.AppDataBase
 import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
+import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class ListaProdutosActivity : AppCompatActivity() {
 
-    private val dao = ProdutosDao()
-    private val adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
-    private val binding by lazy {
-        ActivityListaProdutosActivityBinding.inflate(layoutInflater)
-    }
+   private val dao = ProdutosDao()
+   private val adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+   private val binding by lazy {
+      ActivityListaProdutosActivityBinding.inflate(layoutInflater)
+   }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        configuraRecyclerView()
-        configuraFab()
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(binding.root)
+      configuraRecyclerView()
+      configuraFab()
 
-    }
+      val db = Room.databaseBuilder(
+         this,
+         AppDataBase::class.java,
+         "orgs.db"
+      ).allowMainThreadQueries()
+         .build()
+      val produtoDao = db.produtoDao()
+      produtoDao.salva(
+         Produto(
+            nome = "Teste4",
+            descricao = "teste desc 1",
+            valor = BigDecimal("10.0")
+         )
+      )
+      adapter.atualiza(produtoDao.buscaTodos())
+   }
 
-    override fun onResume() {
-        super.onResume()
-        adapter.atualiza(dao.buscaTodos())
-    }
+   override fun onResume() {
+      super.onResume()
+      //adapter.atualiza(dao.buscaTodos())
+   }
 
-    private fun configuraFab() {
-        val fab = binding.activityListaProdutosFab
-        fab.setOnClickListener {
-            vaiParaFormularioProduto()
-        }
-    }
+   private fun configuraFab() {
+      val fab = binding.activityListaProdutosFab
+      fab.setOnClickListener {
+         vaiParaFormularioProduto()
+      }
+   }
 
-     private fun vaiParaFormularioProduto() {
-        val intent = Intent(this, FormularioProdutoActivity::class.java)
-        startActivity(intent)
-    }
+   private fun vaiParaFormularioProduto() {
+      val intent = Intent(this, FormularioProdutoActivity::class.java)
+      startActivity(intent)
+   }
 
-    private fun configuraRecyclerView() {
-        val recyclerView = binding.activityListaProdutosRecyclerView
-        recyclerView.adapter = adapter
-        adapter.quandoClicaNoItem = {
-            val intent = Intent(
-                this,
-                DetalhesProdutoActivity::class.java
-            ).apply {
-                putExtra(CHAVE_PRODUTO, it)
-            }
-            startActivity(intent)
-        }
-    }
+   private fun configuraRecyclerView() {
+      val recyclerView = binding.activityListaProdutosRecyclerView
+      recyclerView.adapter = adapter
+      adapter.quandoClicaNoItem = {
+         val intent = Intent(
+            this,
+            DetalhesProdutoActivity::class.java
+         ).apply {
+            putExtra(CHAVE_PRODUTO, it)
+         }
+         startActivity(intent)
+      }
+   }
 
 }
